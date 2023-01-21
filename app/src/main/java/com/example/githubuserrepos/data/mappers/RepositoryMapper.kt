4aa02f5c53e645e45958516a11b3538e8228070b
Sergey <sergiksavonow82@gmail.com>
2023-01-21 12:34:04 +0300

    Created splash screen & bugs fix

--- app/src/main/java/com/example/githubuserrepos/data/mappers/RepositoryMapper.kt
+++ app/src/main/java/com/example/githubuserrepos/data/mappers/RepositoryMapper.kt
@@ -13,29 +13,30 @@ class RepositoryMapper{
 
     fun mapNetworkModelToEntity(repositoryNetworkModel: RepositoryNetworkModel): RepositoryEntity{
         val ownerEntity = OwnerEntity(
-            checkEmptyString(repositoryNetworkModel.owner?.avatar_url),
-            checkEmptyString(repositoryNetworkModel.owner?.html_url),
-            checkEmptyInt(repositoryNetworkModel.owner?.id),
-            checkEmptyString(repositoryNetworkModel.owner?.login),
-            checkEmptyString(repositoryNetworkModel.owner?.node_id),
-            checkEmptyString(repositoryNetworkModel.owner?.type),
-            checkEmptyString(repositoryNetworkModel.owner?.url)
+            avatar_url = checkEmptyString(repositoryNetworkModel.owner?.avatar_url),
+            html_url = checkEmptyString(repositoryNetworkModel.owner?.html_url),
+            id = checkEmptyInt(repositoryNetworkModel.owner?.id),
+            login = checkEmptyString(repositoryNetworkModel.owner?.login),
+            node_id = checkEmptyString(repositoryNetworkModel.owner?.node_id),
+            type = checkEmptyString(repositoryNetworkModel.owner?.type),
+            url = checkEmptyString(repositoryNetworkModel.owner?.url)
         )
         return RepositoryEntity(
-            checkEmptyString(repositoryNetworkModel.archive_url),
-            checkEmptyString(repositoryNetworkModel.clone_url),
-            checkEmptyString(repositoryNetworkModel.description),
-            checkEmptyString(repositoryNetworkModel.full_name),
-            checkEmptyString(repositoryNetworkModel.git_url),
-            checkEmptyString(repositoryNetworkModel.html_url),
-            checkEmptyInt(repositoryNetworkModel.id),
-            checkEmptyString(repositoryNetworkModel.language),
-            checkEmptyString(repositoryNetworkModel.name),
-            checkEmptyString(repositoryNetworkModel.node_id),
-            ownerEntity,
-            checkEmptyBoolean(repositoryNetworkModel.private),
-            checkEmptyString(repositoryNetworkModel.ssh_url),
-            checkEmptyString(repositoryNetworkModel.url),
+            archive_url = checkEmptyString(repositoryNetworkModel.archive_url),
+            clone_url = checkEmptyString(repositoryNetworkModel.clone_url),
+            description = checkEmptyString(repositoryNetworkModel.description),
+            full_name = checkEmptyString(repositoryNetworkModel.full_name),
+            git_url = checkEmptyString(repositoryNetworkModel.git_url),
+            html_url = checkEmptyString(repositoryNetworkModel.html_url),
+            id = checkEmptyInt(repositoryNetworkModel.id),
+            language = checkEmptyString(repositoryNetworkModel.language),
+            name = checkEmptyString(repositoryNetworkModel.name),
+            node_id = checkEmptyString(repositoryNetworkModel.node_id),
+            owner = ownerEntity,
+            private = checkEmptyBoolean(repositoryNetworkModel.private),
+            ssh_url = checkEmptyString(repositoryNetworkModel.ssh_url),
+            url = checkEmptyString(repositoryNetworkModel.url),
+            defaultBranch = checkEmptyString(repositoryNetworkModel.default_branch),
         )
     }
 
@@ -63,52 +64,54 @@ class RepositoryMapper{
 
     fun mapDbModelToEntity(repositoryDbModel: RepositoryDbModel): RepositoryEntity{
         val ownerEntity = OwnerEntity(
-            repositoryDbModel.avatarUrl,
-            repositoryDbModel.htmlUrlOwner,
-            repositoryDbModel.idOwner,
-            repositoryDbModel.author,
-            repositoryDbModel.nodeIdOwner,
-            repositoryDbModel.typeOwner,
-            repositoryDbModel.ownerUrl
+            avatar_url = repositoryDbModel.avatarUrl,
+            html_url = repositoryDbModel.htmlUrlOwner,
+            id = repositoryDbModel.idOwner,
+            login = repositoryDbModel.author,
+            node_id = repositoryDbModel.nodeIdOwner,
+            type = repositoryDbModel.typeOwner,
+            url = repositoryDbModel.ownerUrl
         )
         return RepositoryEntity(
-            repositoryDbModel.archiveUrl,
-            repositoryDbModel.cloneUrl,
-            repositoryDbModel.description,
-            repositoryDbModel.fullName,
-            repositoryDbModel.gitUrl,
-            repositoryDbModel.htmlUrl,
-            repositoryDbModel.id,
-            repositoryDbModel.language,
-            repositoryDbModel.name,
-            repositoryDbModel.nodeId,
-            ownerEntity,
-            false,
-            repositoryDbModel.sshIrl,
-            repositoryDbModel.url
+            archive_url = repositoryDbModel.archiveUrl,
+            clone_url = repositoryDbModel.cloneUrl,
+            description = repositoryDbModel.description,
+            full_name = repositoryDbModel.fullName,
+            git_url = repositoryDbModel.gitUrl,
+            html_url = repositoryDbModel.htmlUrl,
+            id = repositoryDbModel.id,
+            language = repositoryDbModel.language,
+            name = repositoryDbModel.name,
+            node_id = repositoryDbModel.nodeId,
+            owner = ownerEntity,
+            private = false,
+            ssh_url = repositoryDbModel.sshIrl,
+            url = repositoryDbModel.url,
+            defaultBranch = repositoryDbModel.defaultBranch
         )
     }
 
     fun mapEntityToDbModel(repositoryEntity: RepositoryEntity) = RepositoryDbModel(
-        repositoryEntity.id,
-        repositoryEntity.name,
-        repositoryEntity.archive_url,
-        repositoryEntity.clone_url,
-        repositoryEntity.description,
-        repositoryEntity.full_name,
-        repositoryEntity.git_url,
-        repositoryEntity.html_url,
-        repositoryEntity.language,
-        repositoryEntity.node_id,
-        repositoryEntity.ssh_url,
-        repositoryEntity.url,
-        repositoryEntity.owner.login,
-        repositoryEntity.owner.avatar_url,
-        repositoryEntity.owner.html_url,
-        repositoryEntity.owner.id,
-        repositoryEntity.owner.node_id,
-        repositoryEntity.owner.type,
-        repositoryEntity.owner.url
+        id = repositoryEntity.id,
+        name = repositoryEntity.name,
+        archiveUrl = repositoryEntity.archive_url,
+        cloneUrl = repositoryEntity.clone_url,
+        description = repositoryEntity.description,
+        fullName = repositoryEntity.full_name,
+        gitUrl = repositoryEntity.git_url,
+        htmlUrl = repositoryEntity.html_url,
+        language = repositoryEntity.language,
+        nodeId = repositoryEntity.node_id,
+        sshIrl = repositoryEntity.ssh_url,
+        url = repositoryEntity.url,
+        defaultBranch = repositoryEntity.defaultBranch,
+        author = repositoryEntity.owner.login,
+        avatarUrl = repositoryEntity.owner.avatar_url,
+        htmlUrlOwner = repositoryEntity.owner.html_url,
+        idOwner = repositoryEntity.owner.id,
+        nodeIdOwner = repositoryEntity.owner.node_id,
+        typeOwner = repositoryEntity.owner.type,
+        ownerUrl = repositoryEntity.owner.url
     )
 
 
