e9e79d44f7318ff267a041f7c9944fb3a0624ee4
Sergey <sergiksavonow82@gmail.com>
2023-01-21 13:02:57 +0300

    Created bottom navigation

--- app/src/main/java/com/example/githubuserrepos/presentation/MainActivity.kt
+++ app/src/main/java/com/example/githubuserrepos/presentation/MainActivity.kt
@@ -3,12 +3,46 @@ package com.example.githubuserrepos.presentation
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
 import com.example.githubuserrepos.R
+import com.example.githubuserrepos.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private val navHostFragment by lazy {
+        supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+    }
+
+    private val navController: NavController by lazy {
+        navHostFragment.navController
+    }
+    private val binding: ActivityMainBinding by lazy {
+        ActivityMainBinding.inflate(layoutInflater)
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         installSplashScreen()
-        setContentView(R.layout.activity_main)
+        setContentView(binding.root)
+        setupNavigation()
+    }
+
+    private fun setupNavigation() {
+        val appBarConfiguration = AppBarConfiguration(
+            setOf(
+                R.id.navigation_search_repositories,
+                R.id.navigation_downloaded_repositories,
+            )
+        )
+        binding.navView.setupWithNavController(navController)
+        setupActionBarWithNavController(navController, appBarConfiguration)
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        return navController.navigateUp() || super.onSupportNavigateUp()
     }
 }
